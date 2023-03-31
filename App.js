19921fded80f65ccb7f834c87771debc31fc4cd7
Eduard Panasenko <eduard.panasenko@itera.com>
2023-03-31 22:52:39 +0300

    init

--- App.js
+++ App.js
@@ -1,21 +1,61 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import {
+  BackdropBlur,
+  Canvas,
+  Fill,
+  Image,
+  useImage,
+  useTouchHandler
+} from "@shopify/react-native-skia";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <BlurImageFilter/>
   );
 }
+const BlurImageFilter = () => {
+  const [points, setPoints] = useState([])
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  const touchHandler = useTouchHandler({
+    onActive: ({ x, y }) => {
+      const value = { x, y };
+      if (!(points.indexOf(value) > -1)) {
+        setPoints([...points, value])
+        points.push(value)
+      }
+    },
+  });
+
+  const image = useImage(require("./image/img.png"));
+  if (!image) {
+    return null;
+  }
+  return (
+    <Canvas style={{ flex: 1 }} onTouch={touchHandler}>
+      <Image
+        x={15}
+        y={100}
+        width={400}
+        height={700}
+        image={image}
+        fit="cover"
+      >
+      </Image>
+      {
+        points.map((value) => {
+          console.log(value.x + " - " + value.y)
+          return (
+            <BackdropBlur blur={4}
+                          clip={{
+                            rect: { x: value.x - 64, y: value.y - 64, width: 128, height: 128 },
+                            rx: 128,
+                            ry: 128
+                          }}>
+              <Fill color="rgba(0, 0, 0, 0)"/>
+            </BackdropBlur>
+          )
+        })
+      }
+    </Canvas>
+  );
+};
