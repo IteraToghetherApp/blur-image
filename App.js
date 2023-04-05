3bc43af882e4d0b2112fdd1c0adf0e1ce012f74e
Eduard Panasenko <eduard.panasenko@itera.com>
2023-04-05 17:33:13 +0300

    improve blur

--- App.js
+++ App.js
@@ -1,27 +1,49 @@
-import React, { useState } from 'react';
+import React from 'react';
 import {
   BackdropBlur,
   Canvas,
   Fill,
+  Group,
   Image,
+  Skia,
   useImage,
   useTouchHandler
 } from "@shopify/react-native-skia";
+import { Dimensions } from 'react-native';
+
+const arr = {};
+
+const pathStart = Skia.Path.Make();
+pathStart.moveTo(128, 0);
+pathStart.lineTo(168, 80);
+pathStart.lineTo(256, 93);
+pathStart.lineTo(192, 155);
+pathStart.lineTo(207, 244);
+pathStart.lineTo(128, 202);
+pathStart.lineTo(49, 244);
+pathStart.lineTo(64, 155);
+pathStart.lineTo(0, 93);
+pathStart.lineTo(88, 80);
+pathStart.lineTo(128, 0);
+pathStart.close();
+
+const path = Skia.Path.Make();
 
 export default function App() {
   return (
     <BlurImageFilter/>
   );
 }
-const BlurImageFilter = () => {
-  const [points, setPoints] = useState([])
+const { height, width } = Dimensions.get('window');
 
+const BlurImageFilter = () => {
   const touchHandler = useTouchHandler({
     onActive: ({ x, y }) => {
-      const value = { x, y };
-      if (!(points.indexOf(value) > -1)) {
-        setPoints([...points, value])
-        points.push(value)
+
+      const strval = `${Math.floor(x)}_${Math.floor(y)}`;
+      if (!arr[strval]) {
+        path.addCircle(Math.floor(x), Math.floor(y), 40)
+        arr[strval] = true;
       }
     },
   });
@@ -33,29 +55,22 @@ const BlurImageFilter = () => {
   return (
     <Canvas style={{ flex: 1 }} onTouch={touchHandler}>
       <Image
-        x={15}
-        y={100}
-        width={400}
-        height={700}
+        x={0}
+        y={0}
+        width={width}
+        height={height}
         image={image}
         fit="cover"
       >
       </Image>
-      {
-        points.map((value) => {
-          console.log(value.x + " - " + value.y)
-          return (
-            <BackdropBlur blur={4}
-                          clip={{
-                            rect: { x: value.x - 64, y: value.y - 64, width: 128, height: 128 },
-                            rx: 128,
-                            ry: 128
-                          }}>
-              <Fill color="rgba(0, 0, 0, 0)"/>
-            </BackdropBlur>
-          )
-        })
-      }
+      <Group>
+
+        <BackdropBlur key={1}
+                      blur={30}
+                      clip={path}>
+          <Fill color="rgba(0, 0, 0, 0)"/>
+        </BackdropBlur>
+      </Group>
     </Canvas>
   );
 };
